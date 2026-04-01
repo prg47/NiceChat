27e340f4f034f90cb626220594bd5395055f96cd
prg47 <115251712+prg47@users.noreply.github.com>
2026-04-01 16:46:37 +0530

    minor fix

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand"
 import { axiosInstance } from "../lib/axios"
+import toast from "react-hot-toast"
 
 export const useChatStore =  create((set,get)=>({
     allContacts: [],
@@ -9,7 +10,7 @@ export const useChatStore =  create((set,get)=>({
     selectedUser : null,
     isUsersLoading : false,
     isMessagesLoading : false,
-    isSoundEnabled : localStorage.getItem("isSoundEnabled") === true,
+    isSoundEnabled : localStorage.getItem("isSoundEnabled") === "true",
 
     toggleSound : ()=>{
         localStorage.setItem("isSoundEnabled",!get().isSoundEnabled)
