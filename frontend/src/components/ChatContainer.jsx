2ba1434563e1a7de3df069b1ce565f0609b7da75
prg47 <115251712+prg47@users.noreply.github.com>
2026-04-06 19:08:42 +0530

    implemented real time messages

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -8,19 +8,24 @@ import MessageInput from "./MessageInput"
 import MessageLoadingSkeleton from "./MessageLoadingSkeleton"
 
 function ChatContainer(){
-    const {selectedUser,getMessagesByUserId,messages,isMessagesLoading} = useChatStore()
+    const {selectedUser,getMessagesByUserId,messages,isMessagesLoading,subscribeToMessages,unsubscribeToMessages} = useChatStore()
     const {authUser} = useAuthStore()
     const messageEndRef = useRef(null)
 
     useEffect(()=>{
         getMessagesByUserId(selectedUser._id)
-    },[selectedUser,getMessagesByUserId])
+        subscribeToMessages()
+
+        return ()=>unsubscribeToMessages()
+    },[selectedUser,getMessagesByUserId,subscribeToMessages,unsubscribeToMessages])
 
      useEffect(() => {
     if (messageEndRef.current) {
       messageEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [messages]);
+
+    
     return(
         <>
             <ChatHeader/>
