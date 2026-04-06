a89ae7b9e3093445cc37701a4d398edd8c89f1d4
prg47 <115251712+prg47@users.noreply.github.com>
2026-04-06 13:53:17 +0530

    added null check

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -29,6 +29,7 @@ function MessageInput() {
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
+    if (!file) return
     if (!file.type.startsWith("image/")) {
       toast.error("Please select an image file");
       return;
